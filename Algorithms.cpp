1a53b800e63507314b9cfe5ea5d1937c4c355592
AdamSinale <121156729+AdamSinale@users.noreply.github.com>
2024-04-27 04:12:50 +0300

    Add files via upload

--- Algorithms.cpp
+++ Algorithms.cpp
@@ -3,36 +3,72 @@
 #include <vector>
 #include <queue>
 
-namespace algorithms {
+#define TRUE 1
+#define FALSE 0
 
-    int bfs(const Graph& graph, int start){
-        int numV = graph.size();
-        queue<int> q;
-        for(int i=1; i<=numV; i++){
-            if(graph.mat[start][i] != 0){
+#define WHITE -1
+#define GRAY 0
+#define BLACK 1
 
+#define INFINITY 2147483647     //maximum integer posibble
+namespace ariel {
+    namespace Algorithms {
+
+        std::vector<int> bfs(const Graph& graph, int start){
+            int numV = graph.getNumV();
+            std::queue<int> q;
+            std::vector<int> colors(numV, WHITE);
+            std::vector<int> dist(numV, INFINITY);
+            std::vector<int> prev(numV, -1);
+            q.push(start);
+            colors[start] = GRAY;
+            dist[start] = 0;
+
+            while(!q.empty()){
+                int u = q.front();
+                q.pop();
+                for(int i=0; i<numV; i++){
+                    if(graph.getEdge(u,i) > 0 && colors[i] == WHITE){
+                        if(dist[i] > dist[u] + graph.getEdge(u,i)){ dist[i] =dist[u] + graph.getEdge(u,i); }
+                        colors[i] = GRAY;
+                        q.push(i);
+                    }
+                }
+                colors[u] = BLACK;
+            }
+            return dist;
+        }
+
+        int isConnected(const Graph& graph){
+            int numV = graph.getNumV();
+            for(int i=0; i<numV; i++){
+                std::vector<int> dist = bfs(graph, i);
+                for(int j=0; j<numV; j++){
+                    if(dist[j] == INFINITY){ return FALSE; }
+                }
+            }
+            return TRUE;
+        }
+
+        int shortestPath(const Graph& graph, int start, int end){
+            std::vector<int> dist = bfs(graph, start);
+            if(dist[end] == INFINITY){ return -1; }
+            return dist[end];
+        }
+
+        int isContainsCycle(const Graph& graph){
+            int numV = graph.getNumV();
+            for(int i=0; i<numV; i++){
+                //   אם ביאפאס יפגוש שחור יש מעגל
             }
         }
+
+        int isBipartite(const Graph& graph){
+
+        }
+
+        int negativeCycle(const Graph& graph){
+
+        }
     }
-
-    int isConnected(const Graph& graph){
-
-    }
-
-    int shortestPath(const Graph& graph, int start, int end){
-
-    }
-
-    int isContainsCycle(const Graph& graph){
-
-    }
-
-    int isBipartite(const Graph& graph){
-
-    }
-
-    int negativeCycle(const Graph& graph){
-
-    }
-
 }
